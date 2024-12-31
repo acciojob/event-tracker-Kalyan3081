ac282e81d4b056544ad9718da3993b782626f4ea
Kalyan3081 <162336115+Kalyan3081@users.noreply.github.com>
2024-12-31 17:44:08 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 
 import React from "react";
-import './../styles/App.css';
+import MyCalendar from './myCalendar'
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <MyCalendar />
     </div>
   )
 }
